c56993e5cbfa92731752490d51804ec30eb1be47
unknown <yonideserbaix@gmail.com>
2023-12-26 14:57:08 +0100

    responsive tablet

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -29,7 +29,7 @@ const DataFetcher = () => {
         loop={true}
         keyboard={{enabled: true,}}
         pagination={{ clickable: true }}
-        autoplay={{ delay: 3000 }}
+        autoplay={{ delay: 5000 }}
       >
         {carouselMovies.map((movie, index) => (
           <SwiperSlide key={index}>
