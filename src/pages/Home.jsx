01cb2e302ca0de096e6c90dcae9fcf30d61f6259
y2_znt <yonideserbaix@gmail.com>
2025-03-22 23:38:57 +0100

    refactor(components): reorganize movie list components into a dedicated moviesList directory and update imports in Home, PopularMoviesPage, and TopRatedPage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import PopularMovieList from "../components/PopularMovieList";
+import PopularMovieList from "../components/moviesList/PopularMovieList";
+import TopRatedMovieList from "../components/moviesList/TopRatedMovieList";
 import Searchbar from "../components/Searchbar";
-import TopRatedMovieList from "../components/TopRatedMovieList";
 
 const Home = () => {
   return (
--- src/pages/TopRatedPage.jsx
+++ src/pages/TopRatedPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Searchbar from "../components/Searchbar";
-import TopRatedMovieList from "../components/TopRatedMovieList";
+import TopRatedMovieList from "../components/moviesList/TopRatedMovieList";
 
 const TopRatedPage = () => {
   return (
