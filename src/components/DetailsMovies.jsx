afe3b008086a8ec12de248dbfc2efe8b7d17ada7
y2_znt <yonideserbaix@gmail.com>
2025-03-22 23:40:12 +0100

    feat(components): enhance DetailsMovies and VideosMovies with animations, loading skeletons, and error handling

--- src/components/DetailsMovies.jsx
+++ src/components/DetailsMovies.jsx
@@ -1,8 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import { motion } from "motion/react";
 import { useNavigate, useParams } from "react-router-dom";
 import { fetchDetailsMovies } from "../api/moviesApi";
 import "../CSS/details.css";
-import SimilarMovies from "./SimilarMovies";
+import FadeOnScroll from "./animations/FadeOnScroll";
+import SimilarMovies from "./moviesList/SimilarMovies";
+import SkeletonDetailsMovies, {
+  ErrorDetailsMovies,
+} from "./skeletons/SkeletonDetailsMovies";
 import VideosMovies from "./VideosMovies";
 
 const DetailsMovies = () => {
@@ -16,10 +21,13 @@ const DetailsMovies = () => {
 
   const goBack = () => {
     navigate(-1);
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
   };
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error fetching movie details: {error.message}</div>;
+  if (isLoading) return <SkeletonDetailsMovies />;
+  if (error) return <ErrorDetailsMovies message={error.message} />;
 
   return (
     <div className="movie">
@@ -28,106 +36,136 @@ const DetailsMovies = () => {
           <i className="fas fa-arrow-left"></i>
         </span>
       </div>
+
       <div className="movie__intro">
-        <img
-          className="movie__backdrop"
-          src={`https://image.tmdb.org/t/p/original${
-            data ? data.backdrop_path : ""
-          }`}
-        />
+        <FadeOnScroll>
+          <img
+            className="movie__backdrop"
+            src={`https://image.tmdb.org/t/p/original${
+              data ? data.backdrop_path : ""
+            }`}
+          />
+        </FadeOnScroll>
       </div>
+
       <div className="movie__detail">
         <div className="movie__detailLeft">
-          <div className="movie__posterBox">
-            <img
-              className="movie__poster"
-              src={`https://image.tmdb.org/t/p/original${
-                data ? data.poster_path : ""
-              }`}
-            />
-          </div>
+          <FadeOnScroll delay={0.4}>
+            <div className="movie__posterBox">
+              <motion.img
+                className="movie__poster"
+                src={`https://image.tmdb.org/t/p/original${
+                  data ? data.poster_path : ""
+                }`}
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
+              />
+            </div>
+          </FadeOnScroll>
         </div>
+
         <div className="movie__detailRight">
-          <div className="movie__detailRightTop">
-            <div className="movie__name">{data ? data.original_title : ""}</div>
-            <div className="movie__tagline">{data ? data.tagline : ""}</div>
-            <div className="movie__rating">
-              ⭐ {data ? data.vote_average : ""}
-              <span className="movie__voteCount">
-                {data ? "(" + data.vote_count + ") votes" : ""}
-              </span>
+          <FadeOnScroll delay={0.6}>
+            <div className="movie__detailRightTop">
+              <div className="movie__name">
+                {data ? data.original_title : ""}
+              </div>
+              <div className="movie__tagline">{data ? data.tagline : ""}</div>
+              <div className="movie__rating">
+                ⭐ {data ? data.vote_average : ""}
+                <span className="movie__voteCount">
+                  {data ? "(" + data.vote_count + ") votes" : ""}
+                </span>
+              </div>
+              <div className="movie__runtime">
+                {data ? data.runtime + " mins" : ""}
+              </div>
+              <div className="movie__releaseDate">
+                {data ? "Release date: " + data.release_date : ""}
+              </div>
+              <div className="movie__genres">
+                {data && data.genres
+                  ? data.genres.map((genre, index) => (
+                      <div key={index}>
+                        <span className="movie__genre" id={genre.id}>
+                          {genre.name}
+                        </span>
+                      </div>
+                    ))
+                  : ""}
+              </div>
             </div>
-            <div className="movie__runtime">
-              {data ? data.runtime + " mins" : ""}
-            </div>
-            <div className="movie__releaseDate">
-              {data ? "Release date: " + data.release_date : ""}
-            </div>
-            <div className="movie__genres">
-              {data && data.genres
-                ? data.genres.map((genre, index) => (
-                    <div key={index}>
-                      <span className="movie__genre" id={genre.id}>
-                        {genre.name}
-                      </span>
-                    </div>
-                  ))
-                : ""}
-            </div>
-          </div>
+          </FadeOnScroll>
+
           <div className="movie__detailRightBottom">
-            <div className="synopsisText">Synopsis</div>
-            <div>{data ? data.overview : ""}</div>
+            <FadeOnScroll delay={0.8}>
+              <div className="synopsisText">Synopsis</div>
+              <div>{data ? data.overview : ""}</div>
+            </FadeOnScroll>
           </div>
         </div>
       </div>
+
       <VideosMovies />
       <SimilarMovies />
-      <div className="movie__links">
-        <div className="movie__heading">Useful Links</div>
-        {data && data.homepage && (
-          <a
-            href={data.homepage}
-            target="_blank"
-            style={{ textDecoration: "none" }}
-          >
-            <p className="movie__homeButton movie__Button">
-              Homepage<i className="newTab fas fa-external-link-alt"></i>
-            </p>
-          </a>
-        )}
-        {data && data.imdb_id && (
-          <a
-            href={"https://www.imdb.com/title/" + data.imdb_id}
-            target="_blank"
-            style={{ textDecoration: "none" }}
-          >
-            <p className="movie__imdbButton movie__Button">
-              IMDb<i className="newTab fas fa-external-link-alt"></i>
-            </p>
-          </a>
-        )}
-      </div>
-      <div className="movie__heading">Production companies</div>
-      <div className="movie__production">
-        {data &&
-          data.production_companies &&
-          data.production_companies.map((company, index) => (
-            <div key={index}>
-              {company.logo_path && (
-                <span className="productionCompanyImage">
-                  <img
-                    className="movie__productionComapany"
-                    src={
-                      "https://image.tmdb.org/t/p/original" + company.logo_path
-                    }
-                  />
-                  <span>{company.name}</span>
-                </span>
-              )}
-            </div>
-          ))}
-      </div>
+
+      <FadeOnScroll>
+        <div className="movie__links">
+          <div className="movie__heading">Useful Links</div>
+          {data && data.homepage && (
+            <motion.a
+              href={data.homepage}
+              target="_blank"
+              style={{ textDecoration: "none" }}
+              whileHover={{ scale: 1.05 }}
+            >
+              <p className="movie__homeButton movie__Button">
+                Homepage<i className="newTab fas fa-external-link-alt"></i>
+              </p>
+            </motion.a>
+          )}
+          {data && data.imdb_id && (
+            <motion.a
+              href={"https://www.imdb.com/title/" + data.imdb_id}
+              target="_blank"
+              style={{ textDecoration: "none" }}
+              whileHover={{ scale: 1.05 }}
+            >
+              <p className="movie__imdbButton movie__Button">
+                IMDb<i className="newTab fas fa-external-link-alt"></i>
+              </p>
+            </motion.a>
+          )}
+        </div>
+      </FadeOnScroll>
+
+      <FadeOnScroll>
+        <div className="movie__heading">Production companies</div>
+        <div className="movie__production">
+          {data &&
+            data.production_companies &&
+            data.production_companies.map((company, index) => (
+              <motion.div
+                key={index}
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.3 }}
+              >
+                {company.logo_path && (
+                  <span className="productionCompanyImage">
+                    <img
+                      className="movie__productionComapany"
+                      src={
+                        "https://image.tmdb.org/t/p/original" +
+                        company.logo_path
+                      }
+                    />
+                    <span>{company.name}</span>
+                  </span>
+                )}
+              </motion.div>
+            ))}
+        </div>
+      </FadeOnScroll>
     </div>
   );
 };
--- src/components/VideosMovies.jsx
+++ src/components/VideosMovies.jsx
@@ -1,47 +1,59 @@
 import { useQuery } from "@tanstack/react-query";
+import { motion } from "motion/react";
 import React from "react";
 import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
 import { fetchVideosMovies } from "../api/moviesApi";
 import "../CSS/details.css";
-// link of library : https://www.npmjs.com/package/react-youtube
-import YouTube from "react-youtube";
+import FadeOnScroll from "./animations/FadeOnScroll";
+import SkeletonVideosMovies, {
+  ErrorVideosMovies,
+} from "./skeletons/SkeletonVideosMovies";
 
 const VideosMovies = () => {
   const { id } = useParams();
 
-  const { data, isLoading, error } = useQuery({
+  const { data, error, isLoading } = useQuery({
     queryKey: ["videosMovies", id],
     queryFn: () => fetchVideosMovies(id),
-    onSuccess: (data) => {
-      if (!data) {
-        throw new Error("Query data cannot be undefined");
-      }
-    },
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error fetching videos movies: {error.message}</div>;
+  if (isLoading) return <SkeletonVideosMovies />;
+  if (error) return <ErrorVideosMovies message={error.message} />;
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
 
   return (
-    <div>
-      {data &&
-        data.slice(0, 4).map((video, index) => (
-          <div key={index}>
-            <div className="video__container">
-              <h2 className="video__title">{video.name}</h2>
-              <div className="video">
-                <YouTube
-                  videoId={video.key}
-                  opts={{
-                    width: "100%",
-                    height: "500px",
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        ))}
-    </div>
+    <FadeOnScroll>
+      <div className="movie__videos">
+        <motion.div
+          className="movie__heading"
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Videos
+        </motion.div>
+        <div className="movie__videos-container">
+          {data &&
+            data.slice(0, 4).map((video) => (
+              <motion.div
+                key={video.id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+              >
+                <YouTube videoId={video.key} opts={opts} />
+              </motion.div>
+            ))}
+        </div>
+      </div>
+    </FadeOnScroll>
   );
 };
 
